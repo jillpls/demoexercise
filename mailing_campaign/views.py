9fccb4cdeaecb785435bd012cd1defccd94e2209
jillpls <jillplspls@gmail.com>
2021-07-13 11:04:53 +0200

    add mailing_lists view skeleton

--- mailing_campaign/views.py
+++ mailing_campaign/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(['GET', 'SET'])
+def mailing_lists(request):
+
+    if request.method == 'GET':
+        pass
+    elif request.method == 'POST':
+        pass
