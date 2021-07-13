d1db6287239b692150ab4402c68da1f0b5bb7ccc
jillpls <jillplspls@gmail.com>
2021-07-13 09:13:53 +0200

    add first draft of Models

--- mailing_campaign/models.py
+++ mailing_campaign/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Video(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    link = models.URLField()  # Link unter dem das Video abrufbar ist
+
+
+class Contact(models.Model):
+    first_name = models.TextField()
+    last_name = models.TextField()
+    email_address = models.TextField()
+
+
+class ContactList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    creation_time = models.DateTimeField()
+    contacts = models.ForeignKey(Contact, many=True, on_delete=models.SET_NULL, null=True)
