299c52df24068c46d2bb4c12924f55c6061bd360
jillpls <jillplspls@gmail.com>
2021-07-13 16:45:24 +0200

    add Campaign class that is not saved to the DB and not a Model

---
+++ mailing_campaign/mail.py
@@ -0,0 +1,15 @@
+from re import template
+
+
+class Campaign(object):
+    video_id = 0
+    template_id = 0 
+    contact_list_id = 0 
+
+    def __init__(self, video_id, template_id, contact_list_id):
+        self.video_id = video_id
+        self.template_id = template_id
+        self.contact_list_id = contact_list_id
+
+    def __str__(self) -> str:
+        return f'[video_id:{self.video_id}, template_id:{self.template_id}, contact_list_id:{self.contact_list_id}]'
